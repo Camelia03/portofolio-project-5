6b35493306ea8cf9f95b04c32a761309515e81ab
Camelia03 <camelia_weber@yahoo.com>
2023-11-03 20:00:02 +0000

    Integrate dj-rest-auth library for login

--- bookworms/settings.py
+++ bookworms/settings.py
@@ -40,11 +40,14 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-REST_USE_JWT = True
-JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'bookworms-auth'
-JWT_AUTH_REFRESH_COOKIE = 'bookworms-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_COOKIE': 'bookworms-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'bookworms-refresh-token',
+}
+# REST_USE_JWT = True
+# JWT_AUTH_SECURE = True
+# JWT_AUTH_SAMESITE = 'None'
 
 # REST_AUTH_SERIALIZERS = {
 #     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
