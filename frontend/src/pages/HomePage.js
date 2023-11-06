a6ee4b0e57e3cef56710e183c3a1e4647b362d87
Camelia03 <camelia_weber@yahoo.com>
2023-11-06 20:49:29 +0000

    Show books in home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
+import { axiosReq } from "../api/axiosDefaults";
 
 const HomePage = () => {
-  const user = useCurrentUser();
-  console.log(user);
+  const [books, setBooks] = useState();
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await axiosReq.get(`/api/books`);
+      setBooks(response.data);
+    };
+    getBooks();
+  }, []);
+
   return (
     <Container>
       <h1>Home Page</h1>
+
+      {books?.results.map((book) => (
+        <div>
+          <p key={book.id}>{book.title}</p>
+          <p>{book.number_of_pages}</p>
+        </div>
+      ))}
     </Container>
   );
 };
