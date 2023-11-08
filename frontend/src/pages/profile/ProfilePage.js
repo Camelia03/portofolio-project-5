7d03451372af5aed0095f3256d2d8c7bb4c661c3
Camelia03 <camelia_weber@yahoo.com>
2023-11-08 17:49:37 +0000

    Style profile page

--- frontend/src/pages/profile/ProfilePage.js
+++ frontend/src/pages/profile/ProfilePage.js
@@ -2,27 +2,87 @@ import React, { useEffect, useState } from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
 import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import styles from "../../styles/ProfilePage.module.css";
+import Loader from "../../components/Loader";
 
 const ProfilePage = () => {
   const currentUser = useCurrentUser();
-  const [profile, setProfile] = useState();
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!currentUser || !currentUser.profile_id) {
+      return;
+    }
+
     const getProfile = async () => {
-      const response = await axiosReq.get(
-        `/api/profiles/${currentUser.profile_id}`
-      );
-      setProfile(response.data);
+      try {
+        const response = await axiosReq.get(
+          `/api/profiles/${currentUser.profile_id}`
+        );
+        setProfile(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
     };
     getProfile();
-  }, []);
+  }, [currentUser]);
+
+  if (loading) {
+    return (
+      <Container className="d-flex justify-content-center align-items-center vh-100">
+        <Loader />
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container className="text-center my-4">
+        <p className="text-danger">Error loading profile data.</p>
+      </Container>
+    );
+  }
 
   return (
-    <Container>
-      <h1>My profile</h1>
-      <p>{profile?.full_name}</p>
-      <img src={profile?.avatar}></img>
-      <p>{profile?.description}</p>
+    <Container className={`text-center ${styles["profile-container"]} my-4`}>
+      <Card bg="light" text="dark">
+        <Card.Body>
+          <Card.Title className="mt-4">My Profile</Card.Title>
+          <div className="my-4">
+            <img
+              src={profile?.avatar}
+              alt="Profile Avatar"
+              className={styles["profile-avatar"]}
+            />
+          </div>
+          <Card.Text className="mb-2">
+            <strong>Full Name:</strong>{" "}
+            {profile?.full_name || "Full Name Not Provided"}
+          </Card.Text>
+          <Card.Text className="mb-2">
+            <strong>Description:</strong>{" "}
+            {profile?.description || "No Description Available"}
+          </Card.Text>
+          <Card.Text className="mb-2">
+            <strong>Joined in:</strong> {profile?.created_at}
+          </Card.Text>
+          <Card.Text className="mb-2">
+            <strong>Updated At:</strong> {profile?.updated_at}
+          </Card.Text>
+          <Button
+            variant="secondary"
+            className={`mb-2 ${styles["edit-profile-button"]}`}
+          >
+            Edit Profile
+          </Button>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
