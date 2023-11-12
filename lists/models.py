356310a95c2efc51cc124dbbcab7d0bc7bf23326
Camelia03 <camelia_weber@yahoo.com>
2023-11-12 18:48:42 +0000

    Create default lists for new users

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,6 @@
-from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from books.models import Book
 
 
@@ -11,4 +11,13 @@ class List(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=False)
     updated_at = models.DateTimeField(auto_now=True, blank=False)
     books = models.ManyToManyField(Book)
-    
+
+
+def create_default_lists(sender, instance, created, **kwargs):
+    if created:
+        List.objects.create(owner=instance, name='want-to-read')
+        List.objects.create(owner=instance, name='reading')
+        List.objects.create(owner=instance, name='read')
+
+
+post_save.connect(create_default_lists, sender=User)
