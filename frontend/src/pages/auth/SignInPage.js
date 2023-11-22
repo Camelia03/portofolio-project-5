2384add86673026ce031a5e76a3e64a7e8749179
Camelia03 <camelia_weber@yahoo.com>
2023-11-22 20:05:10 +0000

    Style sign in/up pages using 2 columns

--- frontend/src/pages/auth/SignInPage.js
+++ frontend/src/pages/auth/SignInPage.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { setTokenTimestamp } from "../../utils/utils";
 import Alert from "react-bootstrap/Alert";
+import { Col, Image, Row } from "react-bootstrap";
+import AppButton from "../../components/AppButton";
 
 function SignInPage() {
   const setCurrentUser = useSetCurrentUser();
@@ -40,38 +41,51 @@ function SignInPage() {
 
   return (
     <Container>
-      <h1>Sign In Page</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="User name"
-            name="username"
-            onChange={handleChange}
-          />
-        </Form.Group>
+      <Row
+        style={{ minHeight: "calc(100vh - 61px)" }}
+        className="justify-content-center align-items-center"
+      >
+        <Col className="h-100">
+          <Image src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382993429i/778267.jpg" />
+        </Col>
 
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-          />
-        </Form.Group>
+        <Col>
+          <h1>Sign In Page</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="User name"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Log in
-        </Button>
+            <Form.Group className="mb-3" controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-        {errors.non_field_errors?.map((message, idx) => (
-          <Alert key={idx} variant="warning" className="mt-3">
-            {message}
-          </Alert>
-        ))}
-      </Form>
+            <AppButton variant="primary" type="submit">
+              Log in
+            </AppButton>
+
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- frontend/src/pages/auth/SignUpPage.js
+++ frontend/src/pages/auth/SignUpPage.js
@@ -4,6 +4,8 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { Col, Image, Row } from "react-bootstrap";
+import AppButton from "../../components/AppButton";
 
 function SignUpPage() {
   const history = useHistory();
@@ -32,55 +34,66 @@ function SignUpPage() {
 
   return (
     <Container>
-      <h1>Sign Up Page</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            onChange={handleChange}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <Row
+        style={{ minHeight: "calc(100vh - 61px)" }}
+        className="justify-content-center align-items-center"
+      >
+        <Col className="h-100">
+          <Image src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382993429i/778267.jpg" />
+        </Col>
 
-        <Form.Group className="mb-3" controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="User name"
-            name="username"
-            onChange={handleChange}
-          />
-        </Form.Group>
+        <Col>
+          <h1>Sign Up Page</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={handleChange}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password1"
-            onChange={handleChange}
-          />
-        </Form.Group>
+            <Form.Group className="mb-3" controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="User name"
+                name="username"
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formConfirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm password"
-            name="password2"
-            onChange={handleChange}
-          />
-        </Form.Group>
+            <Form.Group className="mb-3" controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <Form.Group className="mb-3" controlId="formConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                name="password2"
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <AppButton variant="primary" type="submit">
+              Submit
+            </AppButton>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 }
