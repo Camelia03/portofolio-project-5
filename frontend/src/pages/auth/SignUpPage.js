b85e841c73332959afd87b3698d6acad6f7c1200
Camelia03 <camelia_weber@yahoo.com>
2023-11-24 20:15:03 +0000

    Add validation to sign up page

--- frontend/src/pages/auth/SignUpPage.js
+++ frontend/src/pages/auth/SignUpPage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Col, Image, Row } from "react-bootstrap";
+import { Alert, Col, Image, InputGroup, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import { useHistory } from "react-router-dom";
@@ -19,11 +19,21 @@ function SignUpPage() {
     password2: "",
   });
 
+  const [errors, setErrors] = useState({});
+
   const { username, email, password1, password2 } = signupData;
   const handleChange = (event) => {
     setSignupData({
       ...signupData,
       [event.target.name]: event.target.value,
+    });
+
+    setErrors((prevErrors) => {
+      if (!prevErrors[event.target.name]) return prevErrors;
+
+      const errors = { ...prevErrors };
+      delete errors[event.target.name];
+      return errors;
     });
   };
 
@@ -37,7 +47,9 @@ function SignUpPage() {
         header: "Welcome to BookWorms!",
       });
       history.push("/signin");
-    } catch (error) {}
+    } catch (error) {
+      setErrors(error.response?.data);
+    }
   };
 
   return (
@@ -52,53 +64,94 @@ function SignUpPage() {
 
         <Col>
           <h1>Sign Up Page</h1>
-          <Form onSubmit={handleSubmit}>
+          <Form noValidate onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formEmail">
               <Form.Label>Email address</Form.Label>
+
               <Form.Control
                 type="email"
                 placeholder="Enter email"
                 name="email"
                 onChange={handleChange}
+                isInvalid={!!errors.email}
               />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+
+              {!errors.email && (
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              )}
+
+              {errors.email?.map((message, idx) => (
+                <Form.Control.Feedback key={idx} type="invalid">
+                  {message}
+                </Form.Control.Feedback>
+              ))}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formUsername">
               <Form.Label>Username</Form.Label>
+
               <Form.Control
                 type="text"
                 placeholder="User name"
                 name="username"
                 onChange={handleChange}
+                isInvalid={!!errors.username}
               />
+
+              {errors.username?.map((message, idx) => (
+                <Form.Control.Feedback key={idx} type="invalid">
+                  {message}
+                </Form.Control.Feedback>
+              ))}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formPassword">
               <Form.Label>Password</Form.Label>
+
               <Form.Control
                 type="password"
                 placeholder="Password"
                 name="password1"
                 onChange={handleChange}
+                isInvalid={!!errors.password1}
               />
+
+              {errors.password1?.map((message, idx) => (
+                <Form.Control.Feedback key={idx} type="invalid">
+                  {message}
+                </Form.Control.Feedback>
+              ))}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formConfirmPassword">
               <Form.Label>Confirm Password</Form.Label>
+
               <Form.Control
                 type="password"
                 placeholder="Confirm password"
                 name="password2"
                 onChange={handleChange}
+                isInvalid={!!errors.password2}
               />
+
+              {errors.password2?.map((message, idx) => (
+                <Form.Control.Feedback key={idx} type="invalid">
+                  {message}
+                </Form.Control.Feedback>
+              ))}
             </Form.Group>
 
             <AppButton variant="primary" type="submit">
               Submit
             </AppButton>
+
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
           </Form>
         </Col>
       </Row>
