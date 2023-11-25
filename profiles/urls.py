af31df805b895609d520c91778a7df5e8bb2b970
Camelia03 <camelia_weber@yahoo.com>
2023-11-25 16:22:54 +0000

    Remove trailing slash from get profile endpoint

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+    path('profiles/<int:pk>', views.ProfileDetail.as_view()),
 ]
